9cccdc03ba27f37be987ff5873bf6fc67f73d0c6
Alexey3439 <gorrus39@mail.ru>
2024-03-30 17:00:52 +0300

    in process

--- test/policies/bulletin_policy_test.rb
+++ test/policies/bulletin_policy_test.rb
@@ -4,28 +4,26 @@ class BulletinePolicyTest < ActionDispatch::IntegrationTest
   include Pundit::Authorization
 
   setup do
-    # @account = build_stubbed(:account)
     @admin = users(:admin)
-    # @employee = build_stubbed(:employee, account: @account)
-    # @post = build_stubbed(:post, account: @account)
     @bulletin = bulletins(:one)
   end
-  # bulletin_actions = %i[
-  #   show
-  #   new
-  #   create
-  #   edit
-  #   update
-  #   to_moderate
-  #   publish
-  #   reject
-  #   archive
-  # ]
+
   test 'bulletin actions for an administrator' do
     sign_in @admin
 
     assert policy(@bulletin).show?
-    assert_not false
-    # assert policy(@bulletin).edit?
+    assert_not policy(@bulletin).edit?
   end
 end
+
+# bulletin_actions = %i[
+#   show
+#   new
+#   create
+#   edit
+#   update
+#   to_moderate
+#   publish
+#   reject
+#   archive
+# ]
