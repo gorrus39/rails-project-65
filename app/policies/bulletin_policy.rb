970327fe429c2625e4697b9e92af4a9795f9a690
gorrus39 <gorrus100@gmail.com>
2024-04-02 13:47:29 +0200

    check

--- app/policies/bulletin_policy.rb
+++ app/policies/bulletin_policy.rb
@@ -34,7 +34,7 @@ class BulletinPolicy < ApplicationPolicy
   def archive?
     may_archive = record.may_archive? # aasm позволяет
     return true if user.admin? && may_archive
-    return true if record.user != user # объявление созданное этим юзером
+    return true if record.user == user # объявление созданное этим юзером
 
     false
   end
--- app/policies/web/admin/bulletin_policy.rb
+++ app/policies/web/admin/bulletin_policy.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
-module Web
-  module Admin
-    class BulletinPolicy < ApplicationPolicy
-      def archive?
-        may_archive = record.may_archive? # aasm позволяет
-        return true if user.admin? && may_archive
-        return true if record.user != user # объявление созданное этим юзером
+module Admin
+  class BulletinPolicy < ApplicationPolicy
+    def archive?
+      may_archive = record.may_archive? # aasm позволяет
+      return true if user.admin? && may_archive
+      return true if record.user != user # объявление созданное этим юзером
 
-        false
-      end
+      false
     end
   end
 end
