8ab48ecef7e0b30d3213e23e9ff8c3e7045328eb
gorrus39 <gorrus100@gmail.com>
2024-04-02 17:19:56 +0200

    Render

--- db/migrate/20240329101437_make_admin_status_to_my_account.rb
+++ db/migrate/20240329101437_make_admin_status_to_my_account.rb
@@ -4,6 +4,6 @@ class MakeAdminStatusToMyAccount < ActiveRecord::Migration[7.1]
   def change
     my_email = 'gorrus100@gmail.com'
     user = User.find_or_create_by(email: my_email)
-    user&.update(admin: true)
+    user.update(admin: true)
   end
 end
